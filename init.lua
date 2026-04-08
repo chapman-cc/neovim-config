6f39a071f25b18f56ddd5ca756027238f13473c2
Chapman Cheng <chapboy1989@gmail.com>
2026-04-08 21:05:32 +0100

    add blick.cmp

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ vim.pack.add{
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" }
+    { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1.0") },
 }
 
 
