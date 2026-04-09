9265e993a6cf03a2ed5e89a3aa8d2d91721a5c8b
Chapman Cheng <chapboy1989@gmail.com>
2026-04-09 01:37:09 +0100

    fix: missing mason list

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,3 +1,24 @@
+local server_list = {
+    -- LUA
+    "lua_ls",
+    -- html
+    "html",
+    -- CSS
+    "cssls",
+    "tailwindcss",
+    -- Javascript / Typescript
+    "oxlint",
+    "vtsls",
+    -- Rust
+    -- "bacon-ls", "bacon",
+    -- Bash
+    "bashls",
+    -- misc
+    "jsonls",
+    "yamlls",
+    "marksman",
+}
+
 require("mason").setup({
     ui = {
         icons = {
@@ -9,5 +30,10 @@ require("mason").setup({
 })
 
 require("mason-lspconfig").setup({
-     ensure_installed = { "lua_ls" }
+    ensure_installed = server_list
 })
+
+for _, server in ipairs(server_list) do
+    vim.lsp.enable(server)
+end
+
