ac467e4d606ac34972808716825ac00030f37459
Chapman Cheng <chapman.cheng@eeze.com>
2026-04-14 09:03:58 +0100

    chore: recorder plugin install order

--- init.lua
+++ init.lua
@@ -5,17 +5,17 @@ vim.g.maplocalleader = " "
 -- Install Plugins
 vim.pack.add({
 	{ src = "https://github.com/folke/tokyonight.nvim" },
+	-- lsp
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	-- completions
 	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1.0") },
-	-- additional blink.cmp providers
 	{ src = "https://github.com/disrupted/blink-cmp-conventional-commits" },
 	{ src = "https://github.com/alexandre-abrioux/blink-cmp-npm.nvim" },
 	{ src = "https://github.com/moyiz/blink-emoji.nvim" },
 
-    { src = "https://github.com/folke/which-key.nvim" },
-
+	{ src = "https://github.com/folke/which-key.nvim" },
 	-- nvim-tree
 	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
--- lsp/lua_ls.lua
+++
@@ -1,4 +0,0 @@
-return {
-
-}
--- /Users/chapman/.local/share/nvim/site/pack/core/opt/blink.cmp/lua/blink/cmp/types.lua
