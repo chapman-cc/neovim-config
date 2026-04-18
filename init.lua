0f7db5f9d885f0b113849ac566422db71c9f25af
Chapman Cheng <chapboy1989@gmail.com>
2026-04-18 01:32:37 +0100

    add telescope

--- init.lua
+++ init.lua
@@ -29,6 +29,10 @@ local lazy_modules = {
   -- nvim-tree
   {	src = "https://github.com/nvim-tree/nvim-tree.lua", },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  -- telescope
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
 }
 
 vim.pack.add(eager_modules, { load = true })
