28d969302093a98f13cc94aa367b154cfea47742
Chapman Cheng <chapman.cheng@eeze.com>
2026-04-21 10:45:54 +0100

    feat(conform): add a bunch of formatters to conform setting

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,6 +1,22 @@
 require("conform").setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback"
-  }
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+		quiet = false,
+		stop_after_first = true,
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		json = { "biome", "prettier" },
+		markdown = { "prettier" },
+		javascript = { "biome", "prettier" },
+		typescript = { "biome", "prettier" },
+		javascriptreact = { "biome", "prettier" },
+		typescriptreact = { "biome", "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+	},
+	log_level = vim.log.levels.WARN,
+	notify_on_error = true,
+	notify_no_formatters = true,
 })
